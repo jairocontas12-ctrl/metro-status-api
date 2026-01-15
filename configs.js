156570e4cfd0e118ac19760d9ff88b4cc9bfc590
jairocontas12-ctrl <jairocontas12@gmail.com>
2026-01-15 08:40:06 -0300

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,5 +1,5 @@
 // Configurações da API
 module.exports = {
-  url: 'https://www.metro.sp.gov.br/sistemas/direto-do-metro-via4/diretodometro.asp',
-  cacheDuration: 60000 // 1 minuto
+  METRO_URL: "https://www.metro.sp.gov.br/sistemas/direto-do-metro-via4/diretodometro.asp",
+  CACHE_DURATION: 60000 // 1 minuto
 };
