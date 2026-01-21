41387d672ac57eea80f7c29dd48c5e3f8c0d6ea0
jairocontas12-ctrl <jairocontas12@gmail.com>
2026-01-21 13:20:05 -0300

    Update configs.js

--- configs.js
+++ configs.js
@@ -3,8 +3,15 @@ module.exports = {
   // Fonte oficial (CCM/ARTESP) - Status Metroferroviário (Metrô + CPTM + concessões)
   CCM_STATUS_URL: "https://ccm.artesp.sp.gov.br/metroferroviario/status-linhas/",
 
-  // Cache recomendado (30s ~ 60s)
+  // Fonte oficial (CCM/ARTESP) - Ocorrências (onde geralmente aparece o "motivo" quando há falha)
+  CCM_OCCURRENCES_URL: "https://ccm.artesp.sp.gov.br/metroferroviario/ocorrencias/",
+
+  // Cache do STATUS (30s ~ 60s)
   CACHE_DURATION: 30000,
+
+  // Cache do MOTIVO (separado, para não bater no CCM a cada request)
+  // Pode deixar igual ao cache do status ou um pouco maior.
+  REASON_CACHE_DURATION: 45000,
 
   // Timeout de requisição
   REQUEST_TIMEOUT: 15000,
